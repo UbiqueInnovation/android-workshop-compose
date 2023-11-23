2154896c3f3b79d007c9fe00a9a64097ae6abe2f
Cléa Benz <benz@ubique.ch>
2023-11-23 09:38:35 +0100

    add state

--- app/src/main/java/ch/ubique/workshop/common/util/Playground.kt
+++ app/src/main/java/ch/ubique/workshop/common/util/Playground.kt
@@ -3,6 +3,7 @@ package ch.ubique.workshop.common.util
 import android.widget.ProgressBar
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.CheckCircle
@@ -11,8 +12,10 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -46,6 +49,19 @@ fun ProgressIndicator() {
 	CircularProgressIndicator()
 }
 
+@Composable
+fun MyComonent() {
+	var counter = remember { mutableStateOf(0) }
+	Button(onClick = { counter.value++ }) {
+		MyText(text = "I've been clicked ${counter.value} times")
+	}
+}
+
+@Composable
+fun MyText(text: String) {
+	Text(text = text, Modifier.padding(8.dp))
+}
+
 
 @Preview(showBackground = true)
 @Composable
@@ -57,4 +73,10 @@ fun DefaultPreview() {
 		ButtonTest(onButtonClicked = {})
 		ProgressIndicator()
 	}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyComponentPreview() {
+	MyComonent()
 }
